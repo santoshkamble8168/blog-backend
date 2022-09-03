12a3a0049fc4d3f5e9bf68cb6726a6ecc02ee091
Santosh kamble <santoshkamble8168@gmail.com>
2022-09-03 13:21:35 +0530

    udapte user and password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,40 +1,76 @@
-const {User} = require("../models")
+const { User } = require("../models");
 const { AsyncErrorHandler } = require("../middlewares");
 const { ErrorHandler, Check } = require("../utils");
-const {userValidation} = require("../validations")
-const {messages} = require("../config")
+const { userValidation } = require("../validations");
+const { messages } = require("../config");
 
 exports.createUser = AsyncErrorHandler(async (req, res, next) => {
-  const { error } = userValidation.createUser(req)
+  const { error } = userValidation.createUser(req);
   if (error) return next(new ErrorHandler(error.details, 409));
 
-  const isExist = await Check.isExist(User, {email: req.body.email})
-  if(isExist) return next(new ErrorHandler("Email already registered", 409));
+  const isExist = await Check.isExist(User, { email: req.body.email });
+  if (isExist) return next(new ErrorHandler("Email already registered", 409));
 
   await User.create(req.body);
   //send email for verifation email address
-  
+
   res.status(201).json({
     success: true,
     message: messages.user.create,
   });
 });
 
-exports.updateUser = AsyncErrorHandler(async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(204).json({
+exports.updateUser = AsyncErrorHandler(async (req, res, next) => {
+  const id = req.params.id;
+  if (!id) return next(new ErrorHandler("User Id not provided", 404));
+
+  const user = Check.isExist(User, id);
+  if (!user) return next(new ErrorHandler("User not provided", 404));
+
+  //email & password not allowed here to update
+  const updateFields = {
+    name: req.body.name,
+    role: req.body.role,
+    avatar: req.body.avatar,
+    status: req.body.status
+  };
+
+  const updateUser = await User.findOneAndUpdate(
+    id,
+    {
+      $set: updateFields,
+    },
+    { new: true }
+  )
+
+  res.status(200).json({
     success: true,
-    item: user,
+    messages: messages.user.update,
+    item: updateUser,
   });
 });
 
-exports.updateUserPassword = AsyncErrorHandler(async(req, res, next) => {
-  const user = await User.create(req.body);
-  res.status(204).json({
+exports.updateUserPassword = AsyncErrorHandler(async (req, res, next) => {
+  const id = req.params.id;
+  if (!id) return next(new ErrorHandler("User Id not provided", 404));
+  
+  if(req.body.password !== req.body.confirmPassword) return next(new ErrorHandler("Password not matched", 401))
+
+  const user = await User.findById(id).select("+password")
+  if (!user) return next(new ErrorHandler("User not found", 404));
+
+  const isPasswordMatched = await user.verifyPassword(req.body.oldPassword);
+  if(!isPasswordMatched) return next(new ErrorHandler("old password is incorrect!", 401))
+
+  user.password = req.body.password
+
+  await user.save()
+
+  res.status(200).json({
     success: true,
-    item: user,
+    message: messages.user.passwordUpdate,
   });
-})
+});
 
 exports.deleteUser = AsyncErrorHandler(async (req, res) => {
   const user = await User.create(req.body);
@@ -47,8 +83,7 @@ exports.deleteUser = AsyncErrorHandler(async (req, res) => {
 exports.getAllUsers = AsyncErrorHandler(async (req, res) => {
   //pagination
   //searching
-  //filters
-  console.log("token", req.cookies);
+  //filters++
   const users = await User.find();
   res.status(200).json({
     success: true,
@@ -57,7 +92,7 @@ exports.getAllUsers = AsyncErrorHandler(async (req, res) => {
 });
 
 exports.getUser = AsyncErrorHandler(async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const user = await User.findById(id);
   res.status(200).json({
     success: true,
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
-const { createUser, updateUser, deleteUser, getAllUsers, getUser } = require("../controllers/userController")
+const { createUser, updateUser, deleteUser, getAllUsers, getUser, updateUserPassword } = require("../controllers/userController")
 
 router.post("/", createUser)
 router.put("/:id", updateUser);
+router.put("/update-password/:id", updateUserPassword)
 router.delete("/:id", deleteUser);
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
